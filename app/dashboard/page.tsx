165d36ac045e0698afcd2a992100e09724fa6b81
Adrian Graça <adrianspk1@gmail.com>
2024-02-22 19:02:14 -0300

    nt

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,10 @@
 
+import "../globals.css";
+
 export default function Dashboard(){
     return(
         <div>
-            <iframe  src="https://lottie.host/embed/195c7514-7cae-40a0-ae79-65af1d4d89fd/AM9Tjbtv5v.json"></iframe>
+            
         </div>
     )
 }
